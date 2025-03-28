636fe0aa328e2e3d501caad5af6207cc766dcd23
Luis J. Montes <luisjavm3@gmail.com>
2025-03-28 17:56:26 -0500

    first commit

---
+++ src/main/java/co/luisjavm3/basic_demo_pipeline/PruebaApplication.java
@@ -0,0 +1,13 @@
+package co.luisjavm3.basic_demo_pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaApplication.class, args);
+	}
+
+}
